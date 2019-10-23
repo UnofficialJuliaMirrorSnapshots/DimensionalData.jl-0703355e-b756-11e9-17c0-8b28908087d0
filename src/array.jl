e1e9189b220ed121d4394527fae50a35e7c5b5ff
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-23 11:00:29 +0000

    Snapshot of branch master taken on 2019-10-23T07:00:28.827-04:00 from "https://github.com/UnofficialJuliaMirror/DimensionalData.jl-0703355e-b756-11e9-17c0-8b28908087d0"

--- src/array.jl
+++ src/array.jl
@@ -4,10 +4,14 @@ const AbDimArray = AbstractDimensionalArray
 
 const StandardIndices = Union{AbstractArray,Colon,Integer}
 
+# Interface methods ############################################################
+
 dims(A::AbDimArray) = A.dims
 label(A::AbDimArray) = ""
 
-# Array interface
+
+# Array interface methods ######################################################
+
 Base.size(A::AbDimArray) = size(parent(A))
 Base.iterate(A::AbDimArray, args...) = iterate(parent(A), args...)
 Base.show(io::IO, A::AbDimArray) = begin
@@ -33,23 +37,26 @@ Base.convert(::Type{Array{T,N}}, A::AbDimArray{T,N}) where {T,N} =
 
 Base.copy(A::AbDimArray) = rebuild(A, copy(parent(A)))
 Base.copy!(dst::AbDimArray, src::AbDimArray) = copy!(parent(src), parent(dst))
+Base.copy!(dst::AbDimArray, src::AbstractArray) = copy!(parent(src), dst)
 
-# Similar. TODO this need a rethink. How do we know what the new dims are?
 Base.BroadcastStyle(::Type{<:AbDimArray}) = Broadcast.ArrayStyle{AbDimArray}()
-# Need to cover a few type signatures to avoid ambiguity with base
-Base.similar(A::AbDimArray, ::Type{T}, I::Vararg{<:Integer}) where T =
-    rebuildsliced(A, similar(parent(A), T, I...), I)
 
 Base.similar(A::AbDimArray) = rebuild(A, similar(parent(A)))
 Base.similar(A::AbDimArray, ::Type{T}) where T = rebuild(A, similar(parent(A), T))
-Base.similar(A::AbDimArray, ::Type{T}, ::Tuple{Int64,Vararg{Int64}}) where T = 
+Base.similar(bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{AbDimArray}}, ::Type{ElType}) where ElType = begin
+    A = find_dimensional(bc)
+    # TODO How do we know what the new dims are?
+    rebuildsliced(A, similar(Array{ElType}, axes(bc)), axes(bc))
+end
+
+# Need to cover a few type signatures to avoid ambiguity with base
+# Don't remove these even though they look redundant
+Base.similar(A::AbDimArray, ::Type{T}, I::Vararg{<:Integer}) where T =
+    rebuildsliced(A, similar(parent(A), T, I...), I)
+Base.similar(A::AbDimArray, ::Type{T}, ::Tuple{Int,Vararg{Int}}) where T = 
     rebuild(A, similar(parent(A), T))
 Base.similar(A::AbDimArray, ::Type{T}, I::Tuple{Union{Integer,OneTo},Vararg{Union{Integer,OneTo},N}}) where {T,N} =
     rebuildsliced(A, similar(parent(A), T, I...), I)
-Base.similar(bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{AbDimArray}}, ::Type{ElType}) where ElType = begin
-    A = find_dimensional(bc)
-    rebuildsliced(A, similar(Array{ElType}, axes(bc)), axes(bc))
-end
 
 @inline find_dimensional(bc::Base.Broadcast.Broadcasted) = find_dimensional(bc.args)
 @inline find_dimensional(ext::Base.Broadcast.Extruded) = find_dimensional(ext.x)
@@ -59,6 +66,8 @@ end
 @inline find_dimensional(A::AbDimArray, rest) = A
 @inline find_dimensional(::Any, rest) = find_dimensional(rest)
 
+
+# Concrete implementation ######################################################
 
 """
 A basic DimensionalArray type
